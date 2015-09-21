e1666fd1e4623b58e29e08b0ad402e103569b895
Michael Aldridge <aldridge.mac@gmail.com>
2015-09-20 23:52:54 -0500

    Refactor loader code
    Allow the loader to work for an arbitrary number of contests.
    Load all data for the contest into a dictionary for convenience,
    this allows the contests to be initialized from a single place.

--- server/loader.py
+++ server/loader.py
@@ -1,36 +1,77 @@
 import logging, glob, os, json, sys, collections
 
 class Loader:
-    def __init__(self, directoryPath):
-        self.problemsDictionary = {}
-
+    def __init__(self, dataPath):
         #allows for logging of messages during execution
         self.logger = logging.getLogger(__name__)
+        self.contests=self.initContests(dataPath)
 
-        #verifies that root directory contains problems directory
+    def initContests(self, path):
+        contests = {}
+
+        #Verify the config directory contains at least one contest
+        contestExists=False
+        if (len(os.listdir(path))>=1) and ("problems" not in os.listdir(path)):
+            contestDirs = os.listdir(path)
+            self.logger.debug("Found the following contest dirs: %s", contestDirs)
+
+            #Drop hidden folders
+            for dir in contestDirs:
+                if dir[0] == '.':
+                    self.logger.debug("Dropping hidden directory %s", dir)
+                    contestDirs.remove(dir)
+
+            #Iterate over remaining folders and create contests
+            for contest in contestDirs:
+                tempContest = {}
+                tempContest["problems"] = self.loadContestProblems(os.path.join(path, contest))
+                tempContest["languages"] = self.loadContestLanguages(os.path.join(path, contest))
+                tempContest["users"] = self.loadContestUsers(os.path.join(path, contest))
+                contests[contest] = tempContest
+        else:
+            self.logger.debug("Found only %s", os.listdir(path))
+
+        return contests
+
+    def loadContestUsers(self, contestDir):
+        with open(os.path.join(contestDir, "passwd.json"), 'r') as f:
+            users = json.load(f)
+            self.logger.info("Loaded %s users for %s", len(users), contestDir)
+            return users
+    
+    def loadContestLanguages(self, contestDir):
+        with open(os.path.join(contestDir, "languages.json"), 'r') as f:
+            langs = json.load(f)
+            for lang in langs:
+                self.logger.info("Loading Support for %s", langs[lang]["name"])
+            return langs
+
+    def loadContestProblems(self, contestDir):
+        self.logger.info("Loading contest %s", contestDir)
+        self.logger.debug("Entering %s, found %s", contestDir, os.listdir(contestDir))
+        #verifies that contest directory contains problems directory
         problemsExists = False
-        try:
-            if "problems" in os.listdir(directoryPath):
-            #builds initializing dictionary
-                self.createProblemsDict(os.path.join(directoryPath, "problems"))
-            else:
-                self.logger.error("No problems directory was found!")
-                sys.exit(1)
-        except:
-            self.logger.critical("No configuration directory found, see README.md: ")
+        if "problems" in os.listdir(contestDir):
+            #retrieve the problems for this contest
+            problems = self.createProblemsDict(os.path.join(contestDir, "problems"))
+        else:
+            self.logger.error("No problems directory was found!")
             sys.exit(1)
+
+        return problems
 
     def createProblemsDict(self, path):
         meta_defaults = {}
-
-        self.meta_defaults = self.metaDefaultsIn(os.path.join(path, "meta_defaults.json"))
+        problems = {}
+        meta_defaults = self.metaDefaultsIn(os.path.join(path, "meta_defaults.json"))
         
         #finds subdirectories of problems dir and makes those the 
         #dictionary keys through passing them to the probStats function
         for problem in os.listdir(path):
             if (os.path.isdir(os.path.join(path,problem)) == True):
-                self.probStats(path, problem, self.meta_defaults)
-
+                problems[problem] = self.probStats(path, problem, meta_defaults)
+        return problems
+                
     def metaDefaultsIn(self, defaultPath):
         """Establishes meta default values by reading in from the meta-defaults.json"""
         defaults = {}
@@ -42,13 +83,12 @@ class Loader:
     def probStats(self, path, problem, meta_defaults):
         """Defines each problem and its attributes within the dictionary"""
         problemPath = os.path.join(path, problem)
-
+        problemDict = {}
         self.logger.info("Loading " + problem);
-        self.problemsDictionary[problem] = {}
 
         #Puts description in the problemList
         with open(os.path.join(problemPath, "description.txt")) as desc:
-            self.problemsDictionary[problem]["desc"] = desc.read()
+            problemDict["desc"] = desc.read()
 
         #Decides whether to use each meta value within the problem directory or
         #the default value if there is no value (null) for the dictionary value
@@ -60,26 +100,26 @@ class Loader:
             if (tmpDict.get(key) == None):
                 tmpDict[key] = meta_defaults[key]
 
-        self.problemsDictionary[problem]["meta"] = tmpDict
+        problemDict["meta"] = tmpDict
 
         #Checks whether the input values exist and, if so, it adds it to the
         #problemList
         try:
             newPath = os.path.join(problemPath, "in.txt")
             open(newPath)
-            self.problemsDictionary[problem]["in"] = newPath
+            problemDict["in"] = newPath
         except:
             self.logger.warning("in.txt does not appear to exist.")
 
         #Add out.txt to problemList
-        self.problemsDictionary[problem]["out"] = os.path.join(problemPath, "out.txt")
+        problemDict["out"] = os.path.join(problemPath, "out.txt")
 
         #Return problem to be stored in the dictionary
-        return
+        return problemDict
 
 if __name__ == "__main__":
     import pprint
     print "Hello"
     logging.basicConfig(level=logging.DEBUG)
     loader = Loader("config")
-    pprint.pprint(loader.problemsDictionary)
+    pprint.pprint(loader.contests)
